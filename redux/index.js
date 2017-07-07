9c07fd7b402764889c1f18c94e4c69b34ec96a6d
NetworkSam <676822037@qq.com>
2017-07-07 15:59:56 +0800

    add redux file

--- redux/index.js
+++ redux/index.js
@@ -1,3 +1,18 @@
-/**
- * Created by samuelcheng on 2017/7/7.
- */
+
+import { createStore } from './redux';
+import $ from 'jquery';
+
+$('document.body').append(`
+  <p id="counter"></p>
+  <button id="increaseBtn">+</button>
+  <button id="decreaseBtn">-</button>
+`);
+
+
+//state是状态树，可以是任意的结构
+//action是一个纯对象{type:'increase'}
+
+let reduce  = (state = {number: 0},action)=>{
+
+};
+createStore();
